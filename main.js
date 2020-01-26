ad644accd6437b0337da7cefd33da842e412ccc6
BKLemonTea <jyf1128@naver.com>
2020-01-26 17:57:31 +0900

    Day_3

--- main.js
+++ main.js
@@ -1,39 +1,77 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url'); // require('url') : url 이라는 Module을 사용할 것이다.
+const fileDir = './Data';
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
-    var title = queryData.id
+    
 
     if(pathname === '/'){
-      fs.readFile(`data/${queryData.id}`, 'utf-8', function(err, description){
-        var template = `
-        <!doctype html>
-        <html>
-        <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-        </head>
-        <body>
-        <h1><a href="/">WEB</a></h1>
-        <ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ul>
-        <h2>${title}</h2>
-        <p>${description}</p>
-        </body>
-        </html>
-        `;
-        response.writeHead(200);
-        //fs.readFileSync(__dirname + _url : 사용자가 접속한 url에 따라서 해당 파일들을 읽어줌
-        //response.end(fs.readFileSync(__dirname + _url)); // 사용자에게 데이터를 전송함
-        response.end(template);
-      });
+      if(queryData.id === undefined){ //undefined 정의되어있지않음
+        fs.readdir(fileDir, function(error, fileList){
+          var title = 'Welcome';
+          var description = 'Hello, Node.js';
+          var list = '<ul>';
+          var count = 0;
+          while(count < fileList.length){
+            list = list + `<li><a href="/?id=${fileList[count]}">${fileList[count]}</a></li>`;
+            count++;
+          }
+          list = list + '</ul>';
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+          </head>
+          <body>
+          <h1><a href="/">WEB</a></h1>
+          ${list}
+          <h2>${title}</h2>
+          <p>${description}</p>
+          </body>
+          </html>
+          `;
+          response.writeHead(200);
+          response.end(template);
+        });
+      } else{
+        fs.readdir(fileDir, function(error, fileList){
+          var list = '<ul>';
+          var count = 0;
+          while(count < fileList.length){
+            list = list + `<li><a href="/?id=${fileList[count]}">${fileList[count]}</a></li>`;
+            count++;
+          }
+          list = list + '</ul>';
+          fs.readFile(`data/${queryData.id}`, 'utf-8', function(err, description){
+            var title = queryData.id;
+            var template = `
+            <!doctype html>
+            <html>
+            <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+            </head>
+            <body>
+            <h1><a href="/">WEB</a></h1>
+            ${list}
+            <h2>${title}</h2>
+            <p>${description}</p>
+            </body>
+            </html>
+            `;
+            response.writeHead(200);
+            //fs.readFileSync(__dirname + _url : 사용자가 접속한 url에 따라서 해당 파일들을 읽어줌
+            //response.end(fs.readFileSync(__dirname + _url)); // 사용자에게 데이터를 전송함
+            response.end(template);
+          });
+        });
+      }
     } else {
       response.writeHead(404);
       response.end('Not found');
