7ac8db2125dc6315534e2900c23248e8ce3a7177
BKLemonTea <jyf1128@naver.com>
2020-02-04 20:18:36 +0900

    Day_8

--- main.js
+++ main.js
@@ -2,36 +2,8 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url'); // require('url') : url 이라는 Module을 사용할 것이다.
 var qs = require('querystring');
+var template = require('./lib/template.js');
 const fileDir = './Data';
-
-function templateHTML(title, list, body, control){
-  return `
-  <!doctype html>
-  <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      ${list}
-      ${control}
-      ${body}
-    </body>
-  </html>
-  `;
-}
-
-function templateList(fileList){
-  var list = '<ul>';
-  var count = 0;
-  while(count < fileList.length){
-    list = list + `<li><a href="/?id=${fileList[count]}">${fileList[count]}</a></li>`;
-    count++;
-  }
-  list = list + '</ul>';
-  return list;
-}
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
@@ -43,22 +15,22 @@ var app = http.createServer(function(request,response){
         fs.readdir(fileDir, function(error, fileList){
           var title = 'Welcome';
           var description = 'Hello, Node.js';
-          var list = templateList(fileList);
-          var template = templateHTML(
+          var list = template.List(fileList);
+          var html = template.HTML(
             title, 
             list, 
             `<h2>${title}</h2>${description}`,
             `<a href="/create">create</a>`
             );
           response.writeHead(200);
-          response.end(template);
+          response.end(html);
         });
       } else{
         fs.readdir(fileDir, function(error, fileList){
           fs.readFile(`data/${queryData.id}`, 'utf-8', function(err, description){
             var title = queryData.id;
-            var list = templateList(fileList);
-            var template = templateHTML(
+            var list = template.List(fileList);
+            var html = template.HTML(
               title, 
               list, 
               `<h2>${title}</h2>${description}`,
@@ -72,15 +44,15 @@ var app = http.createServer(function(request,response){
             response.writeHead(200);
             //fs.readFile(__dirname + _url) : 사용자가 접속한 url에 따라서 해당 파일들을 읽어줌
             //response.end(fs.readFileSync(__dirname + _url)); // 사용자에게 데이터를 전송함
-            response.end(template);
+            response.end(html);
           });
         });
       }
     } else if(pathname === "/create") {
       fs.readdir(fileDir, function(error, fileList){
         var title = 'WEB - create';
-        var list = templateList(fileList);
-        var template = templateHTML(title, list, `
+        var list = template.List(fileList);
+        var html = template.HTML(title, list, `
         <form action="/create_process" method="post"> 
           <p><input type="text" name="title" placeholder="title"></p>
           <p><textarea name="description" placeholder="description"></textarea></p>
@@ -88,7 +60,7 @@ var app = http.createServer(function(request,response){
         </form>
         `, '');
         response.writeHead(200);
-        response.end(template);
+        response.end(html);
       });
     } else if(pathname === '/create_process') {
       var body = '';
@@ -109,8 +81,8 @@ var app = http.createServer(function(request,response){
       fs.readdir(fileDir, function(error, fileList){
         fs.readFile(`data/${queryData.id}`, 'utf-8', function(err, description){
           var title = queryData.id;
-          var list = templateList(fileList);
-          var template = templateHTML(
+          var list = template.List(fileList);
+          var html = template.HTML(
             title, 
             list, 
             `
@@ -124,7 +96,7 @@ var app = http.createServer(function(request,response){
             `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
             );
           response.writeHead(200);
-          response.end(template);
+          response.end(html);
         });
       });
     } else if(pathname === '/update_process') {
